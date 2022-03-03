4ceb35b5bb25c862227e08cb4da3b7f2434d71e5
pfelto <paul.r.felton@gmail.com>
2022-03-03 07:30:49 -0500

    User Story 1: Completed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,26 @@
 function App() {
   return (
-    <div style={{ backgroundColor: "lightgrey", minHeight: "100vh" }}></div>
+    <div
+      style={{
+        backgroundColor: "lightgrey",
+        height: "100vh",
+        minHeight: "100vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <div
+        id="drum-machine"
+        style={{
+          width: "66%",
+          height: "60%",
+          border: "2px solid red",
+        }}
+      >
+        <div></div>
+      </div>
+    </div>
   );
 }
 
